adff9460e79bbdb30c8556e5b9f7346dffc31ef0
hyechan90 <hyechan90@naver.com>
2019-11-19 18:14:00 +0900

    Homework

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,58 +2,97 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  height: 68px;
   width: 100vw;
+  height: 60px;
   display: flex;
-  padding: 20px 58px;
+  flex-direction: row;
+  align-items: center;
+  padding: 2px 40px;
   box-sizing: border-box;
 `;
 
 const Logo = styled.img`
-  height: 100%;
-  margin-right: 30px;
+  width: 70px;
+  margin-right: 23px;
 `;
 
 const NavText = styled.ul`
+  list-style: none;
   display: flex;
   color: white;
-  list-style: none;
   height: 100%;
   flex: 1;
   align-items: center;
+  margin: 8px 0;
+  font-size: 10px;
 `;
 
 const NavTextItem = styled.li`
-  display: flex;
-  flex-direction: column;
+  margin-left: 14px;
+  &:hover {
+    color: #b3b3b3;
+    font-weight: normal;
+  }
 `;
 
 const NavIcon = styled.ul`
   display: flex;
+  align-items: center;
   list-style: none;
   height: 100%;
+  margin: 16px 0;
 `;
 
 const NavIconItem = styled.li`
   width: fit-content;
-  margin-right: 20px;
+  padding: 10px;
   & > img {
-    height: 100%;
+    height: 16px;
   }
 `;
 
-const Banner = props => {
+const Banner = () => {
+  const [num, setNum] = React.useState(0);
+
+  const change = e => {
+    setNum(e);
+  };
+
   return (
     <Wrapper>
       <Logo src="https://cdn.pixelprivacy.com/wp-content/uploads/2017/12/Netflix-Logo-1024x277.png" />
       <NavText>
-        <NavTextItem>홈</NavTextItem>
-        <NavTextItem>TV 프로그램</NavTextItem>
-        <NavTextItem>영화</NavTextItem>
-        <NavTextItem>최신 등록 콘텐츠</NavTextItem>
-        <NavTextItem>내가 찜한 콘텐츠</NavTextItem>
+        <NavTextItem
+          onClick={e => change(0)}
+          style={num === 0 ? { color: '#000fff', fontWeight: 'bold' } : {}}
+        >
+          홈
+        </NavTextItem>
+        <NavTextItem
+          onClick={e => change(1)}
+          style={num === 1 ? { color: '#000fff', fontWeight: 'bold' } : {}}
+        >
+          TV프로그램
+        </NavTextItem>
+        <NavTextItem
+          onClick={e => change(2)}
+          style={num === 2 ? { color: '#000fff', fontWeight: 'bold' } : {}}
+        >
+          영화
+        </NavTextItem>
+        <NavTextItem
+          onClick={e => change(3)}
+          style={num === 3 ? { color: '#000fff', fontWeight: 'bold' } : {}}
+        >
+          최신 등록 콘텐츠
+        </NavTextItem>
+        <NavTextItem
+          onClick={e => change(4)}
+          style={num === 4 ? { color: '#000fff', fontWeight: 'bold' } : {}}
+        >
+          내가 찜한 콘텐츠
+        </NavTextItem>
       </NavText>
-
       <NavIcon>
         <NavIconItem>
           <img src="/search.svg" alt="" />
